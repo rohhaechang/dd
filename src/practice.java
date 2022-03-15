2195a839e2190268c1d8346c8c0a708eea7f7e57
haechang <94176916+rohhaechang@users.noreply.github.com>
2022-03-15 18:32:17 +0900

    Initial commit

---
+++ src/practice.java
@@ -0,0 +1,7 @@
+public class practice {
+    public static void main(String[] args) {
+        String a = "alphabet";
+        String b = a.substring(0, 5);
+        System.out.println(b);
+    }
+}
